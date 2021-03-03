0d218175dcff4ecff12dbe6c6a7dee778d4d4130
Danilo Imparato <imparatodanilo@gmail.com>
2021-03-03 09:46:15 -0300

    included memoization

--- nature.R
+++ nature.R
@@ -1,24 +1,57 @@
 library(tidyverse)
 library(magrittr)
 library(rvest)
+library(here)
 
-wait = 5
+setwd(here())
+
+load_tsv <- TRUE
+tsv <- "C:/Users/danilo/Downloads/nature.tsv"
+
+min_wait = 5
+max_wait = 10
 
 page_urls <- sprintf("https://www.nature.com/opinion?page=%i", 1:50)
 
-pages <- page_urls %>% map(~ {
-  Sys.sleep(wait)
-  read_html(.x)
-})
-
-df <- map_df(pages, ~ {
-  html_nodes(.x, 'a[data-track-label^="article card"][href^="/articles/"]') %>%
-  map_df(~ {
-    data.frame(
-       link  = html_attr(.x, "href")
-      ,title = html_node(.x, ".c-article-item__title") %>% html_text
-      ,type  = html_node(.x, ".c-article-item__article-type") %>% html_text
-      ,date  = html_node(.x, ".c-article-item__date") %>% html_text
-    )
+if(load_tsv) {
+  df <- read_tsv(tsv)
+} else {
+  pages <- page_urls %>% map(~ {
+    Sys.sleep(runif(1, min_wait, max_wait))
+    print(.x)
+    read_html(.x)
   })
-})
+  
+  df <- map_df(pages, ~ {
+    html_nodes(.x, 'a[data-track-label^="article card"][href^="/articles/"]') %>%
+      {
+        data.frame(
+          link  = sprintf("https://www.nature.com%s", html_attr(., "href"))
+          ,title = html_node(., ".c-article-item__title") %>% html_text
+          ,type  = html_node(., ".c-article-item__article-type") %>% html_text
+          ,date  = html_node(., ".c-article-item__date") %>% html_text
+        )
+      }
+  })
+  
+  write_tsv(df, tsv)
+}
+
+
+relevant_patterns <- c(
+   "?"
+   ,"who"
+   ,"why"
+   ,"what"
+   ,"which"
+   ,"where"
+   ,"should"
+   ,"must"
+   ,"can"
+   ,"has"
+   ,"have"
+) %>% fixed(ignore_case = T)
+
+df %<>% mutate(
+  relevant = title %>% map(str_detect, relevant_patterns) %>% map_dbl(sum)
+)
